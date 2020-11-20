faa122d918a83b96e5b40c3d0e572f1e29219d34
Devan Wong <devan.wong@yahoo.com>
2020-11-20 09:56:37 -1000

    week 6 assignment 6.3

--- api-getaway/models/users.js
+++ api-getaway/models/users.js
@@ -29,9 +29,12 @@ const User = module.exports = mongoose.model('User', userSchema);
 module.exports.add = (user, callback) => {
     user.save(callback);
 }
-
-
 module.exports.getById = (id, callback) => {
     var query = {_id: id};
     User.findById(query, callback);
+}
+//finding users by email 
+module.exports.getOne= (e, callback) => {
+    var query = {email: e};
+    User.findOne(query, callback);
 }
--- api-getaway/routes/api-catalog.js
+++ api-getaway/routes/api-catalog.js
@@ -16,8 +16,11 @@ var auth_controller = require('../controllers/authController');
 
 //POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
-
+//POST allow user login request
+router.post('/auth/login', auth_controller.user_login);
 //GET request for verifying user tokens
 router.get('/auth/token', auth_controller.user_token);
+//allow user logout request 
+router.get('/auth/logout', auth_controller.user_logout);
 
 module.exports = router;
