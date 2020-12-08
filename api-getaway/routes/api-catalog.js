8072620abfd6ebf6a216af9157360c7cb12be44b
Devan Wong <devan.wong@yahoo.com>
2020-12-07 17:09:22 -0800

    week 8 assignment 8.4

--- api-getaway/routes/api-catalog.js
+++ api-getaway/routes/api-catalog.js
@@ -11,15 +11,16 @@
  * API Routes
  */
 var express = require('express');
+var checkToken = require('../check-token');
 var router = express.Router();
 var auth_controller = require('../controllers/authController');
 
 //POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
+//GET request for verifying user tokens
+router.get('/auth/token', checkToken, auth_controller.user_token);
 //POST allow user login request
 router.post('/auth/login', auth_controller.user_login);
-//GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
 //allow user logout request 
 router.get('/auth/logout', auth_controller.user_logout);
 
