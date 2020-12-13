43b07a4045e32dd77949fc88408eafc6f4d8e2f6
Devan Wong <devan.wong@yahoo.com>
2020-12-13 11:28:54 -0800

    week 9

--- api-getaway/app.js
+++ api-getaway/app.js
@@ -11,7 +11,7 @@ console.log(header.display('Devan', 'Wong', 'API getaway'));
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -36,7 +36,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
