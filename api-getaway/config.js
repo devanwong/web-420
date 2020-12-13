de09b7cc54e2ee0da25869c0649c9b24db4b8a00
Devan Wong <devan.wong@yahoo.com>
2020-12-13 14:18:52 -0800

    week 9

--- api-getaway/config.js
+++ api-getaway/config.js
@@ -8,6 +8,6 @@
 */
 var config = {};
 config.web= {};
-config.web.port = process.env.PORT || '3000';
+config.web.port = process.env.PORT || '11480';
 config.web.secret = 'topsecret';
 module.exports = config;
